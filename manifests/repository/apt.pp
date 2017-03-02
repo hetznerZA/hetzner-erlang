a98076803354de100850fcafb94a149bc7ca96ee
Willem Basson <willem.basson@hetzner.co.za>
2017-03-02 14:39:57 +0200

    wip

--- manifests/repository/apt.pp
+++ manifests/repository/apt.pp
@@ -13,7 +13,7 @@ class erlang::repository::apt {
     include  => {
       deb => true,
       src => false
-    }
+    },
     key      => {
       id     => '434975BD900CCBE4F7EE1B1ED208507CA14F4FCA',
       server => 'http://packages.erlang-solutions.com/debian/erlang_solutions.asc',
